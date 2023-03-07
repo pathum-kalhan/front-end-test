20768f59060c8b39bf3116324d5bf5ad6fb9ebf3
Pathum Kalhan <pathumsimpson@gmail.com>
2023-03-07 17:37:30 +0530

    final

---
+++ TODO.md
@@ -0,0 +1,9 @@
+Implement windowing technique to improve loading time.
+
+Reduce component re-rendering circles by using memo useMemo hook
+
+Improve sidenav styles
+
+Write unit testing for other components as well
+
+
--- client/src/components/ArticleCard.tsx
+++ client/src/components/ArticleCard.tsx
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material';
 import React from 'react'
 import { Article } from '../types'
-import "../ProductList.css"
+import "../ArticleCard.css"
 var intlNumberFormatValues = ['de-DE', 'currency', 'EUR'];
 
 export var formatter = new Intl.NumberFormat(intlNumberFormatValues[0], {
@@ -12,7 +12,7 @@ export var formatter = new Intl.NumberFormat(intlNumberFormatValues[0], {
 export default function ArticleCard({ article }: { article: Article }) {
  
   return (
-    <Grid item className={'article'} xs={12} sm={6} md={3} lg={3}>
+    <Grid item className={'article'} xs={12} sm={6} md={3} lg={2} sx={{display:"flex",justifyContent:"space-between",flexDirection:"column"}}>
       <img src={article.images[0].path}  className={"article__sections"} alt={article.name} />
       <div className={"article__sections"}>{article.name}</div>
       <div className={"article__sections"}>{formatter.format(article.prices.regular.value / 100)}</div>
